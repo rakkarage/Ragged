c205bb296ae7bc0899d3404eb645e78cf5b14ce6
Rakka Rage <rakkarage@gmail.com>
2019-05-01 10:12:51 -0400

    edit js

--- Ragged/wwwroot/js/edit.js
+++ Ragged/wwwroot/js/edit.js
@@ -1,1 +1,20 @@
-// todo!?
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/editHub").build();
+
+connection.on("ReceiveMessage", function (message) {
+	var message = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	document.getElementById("edit").value = message;
+});
+
+connection.start().catch(function (err) {
+	return console.error(err.toString());
+});
+
+document.getElementById("edit").addEventListener("onchange", function (event) {
+	var message = document.getElementById("edit").value;
+	connection.invoke("SendMessage", message).catch(function (err) {
+		return console.error(err.toString());
+	});
+	event.preventDefault();
+});

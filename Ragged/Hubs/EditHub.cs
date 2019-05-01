0eada5f67d00ed80731de4c0108d09e3367f6890
Rakka Rage <rakkarage@gmail.com>
2019-05-01 14:57:32 -0400

    input event works

--- Ragged/Hubs/EditHub.cs
+++ Ragged/Hubs/EditHub.cs
@@ -4,9 +4,17 @@ namespace Ragged.Hubs
 {
 	public class EditHub : Hub
 	{
-		public async Task SendMessage(string message)
+		public async Task EditMessage1(string message)
 		{
-			await Clients.All.SendAsync("ReceiveMessage", message);
+			await Clients.All.SendAsync("EditedMessage1", message);
+		}
+		public async Task EditMessage2(string message)
+		{
+			await Clients.All.SendAsync("EditedMessage2", message);
+		}
+		public async Task EditMessage3(string message)
+		{
+			await Clients.All.SendAsync("EditedMessage3", message);
 		}
 	}
 }
--- Ragged/wwwroot/js/edit.js
+++ Ragged/wwwroot/js/edit.js
@@ -2,18 +2,40 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/editHub").build();
 
-connection.on("ReceiveMessage", function (message) {
+connection.on("EditedMessage1", function (message) {
 	var value = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-	document.getElementById("edit").value = value;
+	document.getElementById("edit1").value = value;
+});
+connection.on("EditedMessage2", function (message) {
+	var value = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	document.getElementById("edit3").value = value;
+});
+connection.on("EditedMessage3", function (message) {
+	var value = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	document.getElementById("edit3").value = value;
 });
 
 connection.start().catch(function (err) {
 	return console.error(err.toString());
 });
 
-document.getElementById("edit").addEventListener("onchange", function (event) {
-	var value = document.getElementById("edit").value;
-	connection.invoke("SendMessage", value).catch(function (err) {
+document.getElementById("edit1").addEventListener("change", function () {
+	var value = document.getElementById("edit1").value;
+	connection.invoke("EditMessage1", value).catch(function (err) {
+		return console.error(err.toString());
+	});
+	event.preventDefault();
+});
+document.getElementById("edit2").addEventListener("onkeyup", function () {
+	var value = document.getElementById("edit2").value;
+	connection.invoke("EditMessage2", value).catch(function (err) {
+		return console.error(err.toString());
+	});
+	event.preventDefault();
+});
+document.getElementById("edit3").addEventListener("input", function () {
+	var value = document.getElementById("edit3").value;
+	connection.invoke("EditMessage3", value).catch(function (err) {
 		return console.error(err.toString());
 	});
 	event.preventDefault();

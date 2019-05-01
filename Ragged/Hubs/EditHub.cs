e7c4ae2acbb07cd39b75ce93cc4bf9f2bd3e6dd6
Rakka Rage <rakkarage@gmail.com>
2019-05-01 10:54:27 -0400

    x

--- Ragged/Hubs/EditHub.cs
+++ Ragged/Hubs/EditHub.cs
@@ -4,9 +4,9 @@ namespace Ragged.Hubs
 {
 	public class EditHub : Hub
 	{
-		public async Task SendMessage(string user, string message)
+		public async Task SendMessage(string message)
 		{
-			await Clients.All.SendAsync("ReceiveMessage", user, message);
+			await Clients.All.SendAsync("ReceiveMessage", message);
 		}
 	}
 }

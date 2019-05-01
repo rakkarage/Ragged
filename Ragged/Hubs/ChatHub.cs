4fee23b8d1f1c5065bc74287e8300db609b847ac
Rakka Rage <rakkarage@gmail.com>
2019-05-01 19:53:02 -0400

    remove bootstrap and jquery

--- Ragged/Hubs/ChatHub.cs
+++ Ragged/Hubs/ChatHub.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 namespace Ragged.Hubs
 {
+	public static class UserHandler
+	{
+		public static HashSet<string> ConnectedIds = new HashSet<string>();
+	}
 	public class ChatHub : Hub
 	{
 		public async Task SendMessage(string user, string message)
 		{
 			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+		public override async Task OnConnectedAsync()
+		{
+			UserHandler.ConnectedIds.Add(Context.ConnectionId);
+			await Clients.All.SendAsync("ReceiveMessage", Context.ConnectionId, "connected");
+			await base.OnConnectedAsync();
 		}
 	}
 }

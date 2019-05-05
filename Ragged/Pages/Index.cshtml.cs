cf6d71c3c1f9db70221d3bd16549228e574296e2
Rakka Rage <rakkarage@gmail.com>
2019-05-05 13:30:26 -0400

    x

--- Ragged/Pages/Index.cshtml.cs
+++ Ragged/Pages/Index.cshtml.cs
@@ -3,11 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Ragged.Pages
 {
+	[BindProperties]
 	public class IndexModel : PageModel
 	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Group { get; set; }
+		public string Topic { get; set; }
+		public string Status { get; set; }
 		private Gename _gename = new Gename();
-		[ViewData]
-		public string Name => _gename.Name();
-		public void OnGet()	{}
+		public void OnGet()
+		{
+			Name = _gename.Name();
+		}
+		public void OnPost()
+		{
+		}
 	}
 }
